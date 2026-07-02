1299cea739a9cc1540e0e63dcbb7c7a9f9ca633a
Anjum Patel <YOUR_GITHUB_EMAIL>
2026-07-02 22:28:07 +0530

    update main and login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,69 +3,12 @@ import { useNavigate, Link } from "react-router-dom";
 import "../styles/Login.css";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-  const handleLogin = async () => {
-  try {
-    const response = await fetch(
-      "https://ecommerce-backend-production-075f.up.railway.app/api/auth/login",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          email: email.trim(),
-          password: password.trim()
-        })
-      }
-    );
-
-    console.log("STATUS =", response.status);
-console.log("OK =", response.ok);
-
-const text = await response.text();
-console.log("RAW RESPONSE =", text);
-
-  } catch (error) {
-    console.log("LOGIN ERROR:", error);
-  }
-};
-
   return (
-    <div className="login-page">
-      <div className="login-box">
-        <h1>Login</h1>
-
-        <input
-          type="email"
-          placeholder="Enter Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <input
-          type="password"
-          placeholder="Enter Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <button
-  onClick={() => {
-    console.log("LOGIN BUTTON CLICKED");
-    handleLogin();
-  }}
->
-  Login
-</button>
-
-        <p>
-          New user? <Link to="/register">Register</Link>
-        </p>
-      </div>
+    <div>
+      <h1>Test</h1>
+      <button onClick={() => alert("Clicked!")}>
+        CLICK ME
+      </button>
     </div>
   );
 }
