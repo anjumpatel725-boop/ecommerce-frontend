c92a6c3157baca9ae1af629bf38c2f2f0ee8ba4b
Anjum Patel <YOUR_GITHUB_EMAIL>
2026-07-02 18:35:38 +0530

    fix App.jsx errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
-import Orders from "./pages/orders";
+import Orders from "./pages/Orders";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminLogin from "./pages/AdminLogin";
 
