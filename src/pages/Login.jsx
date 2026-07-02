50a70a62a874f50dec7a4e99e61ebf9b27380f1f
Anjum Patel <YOUR_GITHUB_EMAIL>
2026-07-02 22:41:58 +0530

    update css and login and package

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-
+import "../styles/Login.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
