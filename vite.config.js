f3870559d521fd677afe37adbcb0aa94ebc09619
Anjum Patel <YOUR_GITHUB_EMAIL>
2026-07-04 18:10:27 +0530

    vercel update

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === "production"
-    ? "/ecommerce-frontend/"
-    : "/"
+  plugins: [react()]
 });
