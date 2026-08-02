ee0b504c4b139904c8ba927efc4a9067f00afe35
Anjum Patel <YOUR_GITHUB_EMAIL>
2026-08-02 19:10:50 +0530

    Update backend URL

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,6 +7,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
+  
   const handleLogin = async () => {
   try {
     const response = await fetch(
